9fe583cbcaab7c082e6552333481fc1dd20a8092
pmalirz <p.malirz@gmail.com>
2023-05-18 17:28:19 +0200

    Remove unnecessary constructor

--- src/main/java/pl/malirz/web/controllers/Model.java
+++ src/main/java/pl/malirz/web/controllers/Model.java
@@ -3,8 +3,4 @@ package pl.malirz.web.controllers;
 import jakarta.validation.constraints.NotBlank;
 
 record Model(@NotBlank String firstName, @NotBlank String lastName) {
-    Model(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
 }
